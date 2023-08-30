2defc696e725f226350d93e153d0aff0200afdfe
Ruslan <ruslan_feoktistov@mail.ru>
2023-08-30 12:42:27 +0400

    add src

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <IndexComponents/>
+  <BlogComponents/>
+  <BlogdetalisComponents/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import IndexComponents from './components/IndexComponents.vue';
+import BlogComponents from './components/BlogComponents.vue';
+import BlogdetalisComponents from './components/BlogdetalisComponents.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    IndexComponents,
+    BlogComponents,
+    BlogdetalisComponents
+}
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
